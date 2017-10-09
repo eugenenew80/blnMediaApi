376853aa551aaf34857bf468848f916eac9a1f3e
Eugene B <eugenenew80@gmail.com>
2017-10-09 20:09:16 +0600

    -

--- src/main/java/kz/kegoc/bln/entity/media/HourlyMeteringDataRaw.java
+++ src/main/java/kz/kegoc/bln/entity/media/HourlyMeteringDataRaw.java
@@ -1,23 +1,16 @@
 package kz.kegoc.bln.entity.media;
 
-import kz.kegoc.bln.converters.LocalDateTimeAdapter;
 import kz.kegoc.bln.entity.common.HasId;
-import kz.kegoc.bln.entity.media.MeteringDataStatus;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import java.time.LocalDateTime;
-
-import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 
 @Data
 @EqualsAndHashCode(of= {"id"})
 public class HourlyMeteringDataRaw implements HasId {
 	private Long id;
-	
-	@XmlJavaTypeAdapter(LocalDateTimeAdapter.class)
 	private LocalDateTime meteringDate;
-	
 	private Byte hour;
 	private String meteringPointCode;
 	private String externalMeteringPointCode;
--- src/main/java/kz/kegoc/bln/util/RedissonClientProducer.java
+++ src/main/java/kz/kegoc/bln/util/RedissonClientProducer.java
@@ -1,19 +1,32 @@
 package kz.kegoc.bln.util;
 
 import javax.enterprise.inject.Produces;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
+import org.redisson.codec.JsonJacksonCodec;
 import org.redisson.config.Config;
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class RedissonClientProducer {
-	
+	private RedissonClient redissonClient = null;
+
 	@Produces
 	public RedissonClient createRedissonClient() {
+		if (redissonClient !=null)
+			return redissonClient;
+
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.registerModule(new JavaTimeModule());
+
 		Config config = new Config();
 		config.useSingleServer().setAddress("redis://127.0.0.1:6379");
-		RedissonClient redisson = Redisson.create(config);		
-		return redisson;
+		config.setCodec(new JsonJacksonCodec(mapper));
+
+		redissonClient = Redisson.create(config);
+		return redissonClient;
 	}
 }
