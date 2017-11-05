f95e15d7f52baf7b563bc612d9ea7d8fc5a268eb
Eugene B <eugenenew80@gmail.com>
2017-11-05 15:54:42 +0600

    -

--- src/main/java/kz/kegoc/bln/entity/common/HasCode.java
+++ src/main/java/kz/kegoc/bln/entity/common/HasCode.java
@@ -2,6 +2,5 @@ package kz.kegoc.bln.entity.common;
 
 public interface HasCode  {
 	String getCode();
-	
 	void setCode(String code);
 }
--- src/main/java/kz/kegoc/bln/entity/common/HasName.java
+++ src/main/java/kz/kegoc/bln/entity/common/HasName.java
@@ -2,6 +2,5 @@ package kz.kegoc.bln.entity.common;
 
 public interface HasName  {
 	String getName();
-	
 	void setName(String name);
 }
