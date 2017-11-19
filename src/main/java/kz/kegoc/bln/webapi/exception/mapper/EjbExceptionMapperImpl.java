15e7c86712bf69c7885baf88404ed17dddab311e
Eugene B <eugenenew80@gmail.com>
2017-11-19 20:45:30 +0600

    -

--- src/main/java/kz/kegoc/bln/webapi/exception/mapper/EjbExceptionMapperImpl.java
+++ src/main/java/kz/kegoc/bln/webapi/exception/mapper/EjbExceptionMapperImpl.java
@@ -1,6 +1,7 @@
 package kz.kegoc.bln.webapi.exception.mapper;
 
 import javax.ejb.EJBException;
+import javax.transaction.RollbackException;
 import javax.validation.ValidationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -8,6 +9,7 @@ import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 import kz.kegoc.bln.exception.ApplicationException;
 import kz.kegoc.bln.webapi.exception.entity.ErrorMessage;
+import org.hibernate.exception.SQLGrammarException;
 
 
 @Provider
@@ -15,9 +17,26 @@ public class EjbExceptionMapperImpl implements ExceptionMapper<EJBException> {
 	
     @Override
     public Response toResponse(EJBException exc) {
-    	
-    	if (exc.getCause()!=null) {
-    		if (exc.getCause() instanceof ValidationException) { 
+		if (exc.getCause()!=null) {
+			if (exc.getCause() instanceof RollbackException) {
+				Throwable appExc =  exc.getCause().getCause().getCause().getCause();
+				//System.out.println("QQQQQ EjbExceptionMapperImpl " + appExc.getClass());
+
+				return Response.status(500)
+					.type(MediaType.APPLICATION_JSON)
+					.entity(new ErrorMessage("tx-exception", appExc.getMessage()))
+					.build();
+			}
+
+			if (exc.getCause() instanceof SQLGrammarException) {
+				Throwable appExc =  exc.getCause().getCause();
+				return Response.status(500)
+					.type(MediaType.APPLICATION_JSON)
+					.entity(new ErrorMessage("sql-exception", appExc.getMessage()))
+					.build();
+			}
+
+			if (exc.getCause() instanceof ValidationException) {
     			ValidationException appExc = (ValidationException) exc.getCause();
     	    	return Response.status(500)
     	    		.type(MediaType.APPLICATION_JSON)	
